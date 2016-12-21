726415fa7c193a98ce154a1cc66653f3148bb9cb
Cwalker924 <cwalker924@gmail.com>
2016-12-21 04:02:03 +0000

    updating _strcmp.c

--- _strcmp.c
+++ _strcmp.c
@@ -8,14 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int j;
+	int i;
 
-	j = 0;
-	while (s1[j] != '\0' && s2[j] != '\0')
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[j] != s2[j])
-			return ((s1[j] - '0') - (s2[j] - '0'));
-		j++;
+		if (s1[i] == '\0')
+			return (1);
+		if (s1[i] != s2[i])
+			return (-1);
 	}
 	return (0);
 }
