407da2e66a3b891be1b769c425f57c3a44ec437c
Cwalker924 <cwalker924@gmail.com>
2016-12-21 02:35:55 +0000

    updating... plus path

--- header.h
+++ header.h
@@ -17,18 +17,6 @@ char *_strcat(char *dest, char *src);
 int fileExists(const char *file);
 int _strcmp(char *s1, char *s2);
 
-/**
- * struct flag -flag cmd
- * @letter: flag char
- * @prnt: print function pointer
- * Descriptions: contains flag letter and corresponding cmd function
- **/
-typedef struct flag
-{
-	char cmd;
-	int (*ex_ec)(va_list *);
-} find_cmd;
-
 /**/
 typedef struct node{
 	char *dirdir;
